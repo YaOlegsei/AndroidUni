cb0b08c1f607253e4a980a4908db53b5c2847633
Alexey <alexey.kolotilo@cruxlab.com>
2020-10-24 19:09:20 +0300

    Add change color button

--- app/src/main/java/com/alexeykolotilo/myuniapp/AnotherActivity.kt
+++ app/src/main/java/com/alexeykolotilo/myuniapp/AnotherActivity.kt
@@ -1,8 +1,10 @@
 package com.alexeykolotilo.myuniapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.layout_another_activity.*
+import kotlin.random.Random
 
 class AnotherActivity : AppCompatActivity() {
 
@@ -14,5 +16,12 @@ class AnotherActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         textView.text = intent?.extras?.getString("myText")
+
+        randomColorButton.setOnClickListener {
+            val red = Random.nextInt(0, 256)
+            val green = Random.nextInt(0, 256)
+            val blue = Random.nextInt(0, 256)
+            colorView.setBackgroundColor(Color.rgb(red, green, blue))
+        }
     }
 }
